a75c8747f25142d34a045f1e3e1155e556e67407
fatemehoushmand <fatemeh997.997@gmail.com>
2024-04-21 01:02:09 +0330

    new

--- homework_4/reverse.py
+++ homework_4/reverse.py
@@ -1,5 +1,7 @@
 my_list= [12,14,54,2,35,25,6]
 
-reverse = my_list[::-1]
+reversed_list = []
+for i in range(len(my_list)-1, -1, -1):
+    reversed_list.append(my_list[i])
+print(reversed_list) 
 
-print(reverse)
